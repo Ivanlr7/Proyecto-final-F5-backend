a13ff06c401053a20687be6ef573047fa74ddc44
Iván Lorenzo <elhibar77@hotmail.es>
2025-10-07 14:47:59 +0200

    Configuración inicial

---
+++ src/main/java/dev/ivan/reviewverso_back/ReviewversoBackApplication.java
@@ -0,0 +1,13 @@
+package dev.ivan.reviewverso_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewversoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewversoBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ivan/reviewverso_back/ReviewversoBackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ivan.reviewverso_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewversoBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
