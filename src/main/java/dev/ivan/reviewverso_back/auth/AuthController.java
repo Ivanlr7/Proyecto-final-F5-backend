ba5bc6774ad83fe16eb0d347c1749530aaade440
Ivan Lorenzo <elhibar77@hotmail.es>
2025-10-08 10:40:54 +0200

    PFF-5 refact: actualizado AuthController para añadir el login más adelante

--- src/main/java/dev/ivan/reviewverso_back/auth/AuthController.java
+++ src/main/java/dev/ivan/reviewverso_back/auth/AuthController.java
@@ -1,6 +1,6 @@
 package dev.ivan.reviewverso_back.auth;
 
-import dev.ivan.reviewverso_back.auth.dto.RegisterRequest;
+import dev.ivan.reviewverso_back.register.dto.RegisterRequestDTO;
 import dev.ivan.reviewverso_back.user.UserEntity;
 import dev.ivan.reviewverso_back.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +11,5 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-    private final UserRepository userRepository;
 
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
-     
-        return ResponseEntity.ok().build();
-    }
 }
